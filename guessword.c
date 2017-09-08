81629422f7d63f54240f9259c38b07c92865661b
Marijan Gradecak <gradecam@tcd.ie>
2017-09-08 14:08:57 +0200

    almost at working version

--- guessword.c
+++ guessword.c
@@ -2,22 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <crypt.h>
+/* #include <crypt.h> */
 
 //forward declrations
 typedef struct Account Account;
-void parse_shadow(char*);
 void read_shadow_passwd(char*, char*);
 void read_dict();
-void cleanup();
+void read_common_passwds(char*);
+void parse_shadow(char*);
 void parse_accounts(FILE*, FILE*);
+Account* parse_accounts_entry(char*, char*);
+void cleanup();
 /******************************************/
 
 //globals
-char* g_salt;
-char** g_dict;
-int g_dict_len;
+char*    g_salt;
+char**   g_dict;
+int      g_dict_len;
 Account* g_accounts;
+int      g_accounts_len;
 /******************************************/
 
 // consts
@@ -27,10 +30,54 @@ const int SALT_DELIMITER = '$';
 /******************************************/
 
 struct Account{
-  char* fullname;
+  char* firstname;
   char* username;
   char* passwd_hash;
 };
+
+Account* parse_accounts_entry(char* shadow_line, char* passwd_line){
+  Account* acc = malloc(sizeof(Account*));
+  acc->username = malloc(sizeof(char*) * 7);  /* 7 chars (username(6) + null(1)) */
+  acc->passwd_hash = malloc(sizeof(char*)*29); /* 28char hash + \0 */
+
+  /* parse entries according to username and hash length patterns*/
+  strncpy(acc->username, passwd_line, 6);
+  strncpy(acc->passwd_hash, &shadow_line[7], 28);
+
+  /* terminate the substrings  */
+  acc->username[6] = '\0';
+  acc->passwd_hash[28] = '\0';
+  return acc;
+}
+
+void parse_accounts(FILE* shadow_f, FILE* passwd_f){
+  int init_accounts_len = 10000;
+  g_accounts = malloc(sizeof(Account*)*init_accounts_len);
+  g_accounts_len = 0;
+  int eof = 0;
+  for(;eof != 1; g_accounts_len++){
+    if(g_accounts_len>= init_accounts_len){
+      int new_len = init_accounts_len + 20000;
+      g_accounts = realloc(g_accounts, sizeof(char*)*new_len);
+      init_accounts_len = new_len;
+    }
+    /* read line */
+    size_t n = 0;
+    char* shadow_entry = NULL;
+    char* passwd_entry = NULL;
+    int res = getline(&shadow_entry, &n, shadow_f);
+    n = 0;
+    int res1 = getline(&passwd_entry, &n, passwd_f);
+    if (res == -1 || res1 == -1){
+      // error reading line or EOF reached
+      eof = 1;
+      g_accounts_len--; // remove last null entry
+    }
+    else{
+      g_accounts[g_accounts_len] = *parse_accounts_entry(shadow_entry, passwd_entry);
+    }
+  }
+}
 
 void set_salt(char* shadow_entry){
   g_salt = malloc(strlen(shadow_entry));
@@ -52,13 +99,13 @@ void read_shadow_passwd(char* shadow_path, char* passwd_path){
   }
 
   char* shadow_line = NULL;
-  char* passwd_line = NULL;
+  /* char* passwd_line = NULL; */
   size_t len = 0;
-  size_t p_len = 0;
-  getline(&shadow_line, &s_len, shadow_file);
+  /* size_t s_len = 0; */
+  getline(&shadow_line, &len, shadow_file);
   rewind(shadow_file);
   set_salt(shadow_line);
-
+  parse_accounts(shadow_file, passwd_file);
 }
 
 void cleanup(){
@@ -106,7 +153,7 @@ int main(int argc, char *argv[] ){
     exit(1);
   }
   read_dict(); // read dict into memory
-  /* read_shadow_passwd(argv[1], argv[2]); */
+  read_shadow_passwd(argv[1], argv[2]);
   /* char* hash = crypt("damien90", g_salt); */
   /* fprintf(stderr, "%s", hash); */
   cleanup();
