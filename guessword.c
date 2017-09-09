e982700fd8695dcca6c8257700f6e6a3f8c864c0
Marijan Gradecak <gradecam@tcd.ie>
2017-09-09 15:36:32 +0200

    now checks top250 passwords against user accounts

--- guessword.c
+++ guessword.c
@@ -1,38 +1,53 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 /* #include <crypt.h> */
 
-//forward declrations
+//Forward declrations
 typedef struct Account Account;
-void read_shadow_passwd(char*, char*);
+typedef struct Password Password;
+typedef enum { false, true } bool;
+void attack(char*, char*);
 void read_dict();
-void read_common_passwds(char*);
+void read_common_passwds();
 void parse_shadow(char*);
 void parse_accounts(FILE*, FILE*);
 Account* parse_accounts_entry(char*, char*);
 void cleanup();
+bool name_permutations(Account*);
 /******************************************/
 
 //globals
-char*    g_salt;
-char**   g_dict;
-int      g_dict_len;
-Account* g_accounts;
-int      g_accounts_len;
+char*     g_salt;
+char**    g_dict;
+Password* g_common_passwds = NULL;
+int       g_dict_len;
+Account*  g_accounts;
+int       g_accounts_len;
 /******************************************/
 
-// consts
+// CONSTS (CONFIG)
 /* const char* DICT_PATH = "./dictionary/gutenberg/dict.txt"; */
-const char* DICT_PATH = "./out.txt";
-const int SALT_DELIMITER = '$';
+const char* DICT_PATH    = "./out.txt";
+const char* TOP_250_PATH = "./dictionary/top250.txt";
+
 /******************************************/
 
 struct Account{
+  bool solved;
   char* firstname;
+  char* lastname;
+  char* middlename;
   char* username;
   char* passwd_hash;
+};
+
+struct Password{
+  char* password;
+  char* hash;
 };
 
 Account* parse_accounts_entry(char* shadow_line, char* passwd_line){
@@ -43,11 +58,30 @@ Account* parse_accounts_entry(char* shadow_line, char* passwd_line){
   /* parse entries according to username and hash length patterns*/
   strncpy(acc->username, passwd_line, 6);
   strncpy(acc->passwd_hash, &shadow_line[7], 28);
-
   /* terminate the substrings  */
   acc->username[6] = '\0';
   acc->passwd_hash[28] = '\0';
+  acc->solved = false;
+  //check user against common passwords
+  for(int i=0; i<250; i++){
+    int res = strcmp(acc->passwd_hash, g_common_passwds[i].hash);
+    if(res == 0){
+      printf("%s:%s\n", acc->username, g_common_passwds[i].password);
+      fflush(stdout);
+      acc->solved = true;
+    }
+  }
+
+  // try combinations of users name
+  if(!acc->solved){
+    bool solved = name_permutations(acc);
+    acc->solved = solved;
+  }
   return acc;
+}
+
+bool name_permutations(Account* acc){
+
 }
 
 void parse_accounts(FILE* shadow_f, FILE* passwd_f){
@@ -74,6 +108,10 @@ void parse_accounts(FILE* shadow_f, FILE* passwd_f){
       g_accounts_len--; // remove last null entry
     }
     else{
+      /* Account* a = parse_accounts_entry(shadow_entry, passwd_entry); */
+      /* g_accounts[g_accounts_len].firstname = a->firstname; */
+      /* g_accounts[g_accounts_len].username = a->username; */
+      /* g_accounts[g_accounts_len].passwd_hash = a->passwd_hash; */
       g_accounts[g_accounts_len] = *parse_accounts_entry(shadow_entry, passwd_entry);
     }
   }
@@ -86,7 +124,7 @@ void set_salt(char* shadow_entry){
   /* fprintf(stderr, "salt %s", g_salt); */
 }
 
-void read_shadow_passwd(char* shadow_path, char* passwd_path){
+void attack(char* shadow_path, char* passwd_path){
   FILE* shadow_file;
   FILE* passwd_file;
 
@@ -105,7 +143,10 @@ void read_shadow_passwd(char* shadow_path, char* passwd_path){
   getline(&shadow_line, &len, shadow_file);
   rewind(shadow_file);
   set_salt(shadow_line);
+
+  read_common_passwds();
   parse_accounts(shadow_file, passwd_file);
+  read_dict();
 }
 
 void cleanup(){
@@ -113,6 +154,38 @@ void cleanup(){
     free(g_dict[i]);
   }
   free(g_dict);
+  free(g_salt);
+  /* for(int i=0; i<251;i++){ */
+  /*   free(g_common_passwds[i]); */
+  /* } */
+  /* free(g_common_passwds); */
+}
+
+void read_common_passwds(){
+  fprintf(stderr, "reading common passwotds\n");
+  FILE* common_pswds;
+  common_pswds = fopen(TOP_250_PATH, "r");
+  if(common_pswds == NULL){
+    fprintf(stderr, "Couldn't open top 250 file for reading\n");
+  }
+  char** passwords = (char**) malloc(sizeof(char*) * 250 );
+  g_common_passwds = malloc(sizeof(Password*) * 250);
+  for(int i=0; i<250; i++){
+    size_t n = 0;
+    int res = getline(&passwords[i], &n, common_pswds);
+    if(res == -1){
+      fprintf(stderr,"error reading line (or eof reached)");
+    }
+  }
+  for(int i=0; i<250; i++){
+    int l = strlen(passwords[i]);
+    g_common_passwds[i].password = malloc(sizeof(char*)* l);
+    strncpy(g_common_passwds[i].password, passwords[i], l-2);
+    struct crypt_data* d = malloc(sizeof(struct crypt_data));
+    d->initialized = 0;
+    g_common_passwds[i].password[l] = '\0';
+    g_common_passwds[i].hash = crypt_r(g_common_passwds[i].password, g_salt, d);
+  }
 }
 
 void read_dict(){
@@ -152,8 +225,8 @@ int main(int argc, char *argv[] ){
     fprintf(stderr, "invalid number of arguments");
     exit(1);
   }
-  read_dict(); // read dict into memory
-  read_shadow_passwd(argv[1], argv[2]);
+  attack(argv[1], argv[2]);
+  // read dict into memory
   /* char* hash = crypt("damien90", g_salt); */
   /* fprintf(stderr, "%s", hash); */
   cleanup();
--- preprocess.py
+++ preprocess.py
@@ -1,18 +1,34 @@
 import sys
 
-def gen_permutations(words):
-  with open('./output.txt', 'w') as out:
-    for word in words:
-      for j in range(len(words)):
-        if(word != words[j]):
-          out.write(word+words[j] + '\n')
+len_11 = []
+len_13 = []
+lessthan_13 = []
 
+def gen_permutations():
+  with open('./output.txt', 'w') as out:
+    out.write('\n'.join(lessthan_13))
+    for word in len_11:
+      for word1 in len_13:
+          out.write(word+word1 + '\n')
+
+def find_words(words):
+  global len_11;
+  global len_13;
+  global lessthan_13;
+  for word in words:
+    if len(word) == 11:
+      len_11.append(word)
+    elif len(word) <= 12:
+      lessthan_13.append(word)
+    elif len(word) == 13:
+      len_13.append(word)
 
 def main():
   lines = []
   with open(sys.argv[1], 'r') as dic:
     lines = dic.read().splitlines()
-    gen_permutations(lines)
+    find_words(lines)
+    gen_permutations()
 
 
 if __name__  == '__main__':
